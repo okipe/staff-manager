7f940503a4c7a7e8e5f4e5a27e43f25ac20d55e8
Oscar Roman <oscarroman.pe@gmail.com>
2024-08-06 12:06:19 -0500

    First App

---
+++ src/main/java/com/mycompany/staff/manager/StaffManager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.staff.manager;
+
+/**
+ *
+ * @author oscar
+ */
+public class StaffManager {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
